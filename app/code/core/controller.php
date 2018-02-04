e91234e528799b01da758d7ca9acca834831ac19
Chris Putnam <chris@webpagefx.com>
2018-02-04 04:14:31 +0000

    Fix routing - no longer dependant on alpha-order

--- app/code/core/controller.php
+++ app/code/core/controller.php
@@ -10,10 +10,18 @@ class Core_Controller extends Core_Controller_Abstract
     static public function route()
     {
         Login_Controller::redirect('/login');
+
+        // Start with _index controller
+        $file_path = DIR_CODE . DS . '_index' . DS . 'controller.php';
+        if (is_file($file_path))
+        {
+            require_once($file_path);
+        }
+
         $code_dir = opendir(DIR_CODE);
         while ($dir = readdir($code_dir))
         {
-            if (in_array($dir, ['.','..']) or !is_dir(DIR_CODE . DS . $dir)) continue;
+            if (in_array($dir, ['.','..','_index']) or !is_dir(DIR_CODE . DS . $dir)) continue;
             $file_path = DIR_CODE . DS . $dir . DS . 'controller.php';
             if (is_file($file_path))
             {
