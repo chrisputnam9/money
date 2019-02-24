4cc95db1a1fee8512795139b9254747ad72956eb
Chris Putnam <chris@webfx.com>
2019-02-24 15:44:59 -0500

    Script tweak

--- local.dev/database/copy_live_to_dev.sh
+++ local.dev/database/copy_live_to_dev.sh
@@ -3,5 +3,5 @@
 livedb="/var/www/money/local.dev/database"
 devdb="/var/www/money-dev/local.dev/database"
 
-c m dump "$devdb/project.sql" "$livedb/dbconfig.php"
-c m dump "$devdb/project.sql" "$devdb/dbconfig.php"
+~/.cmp/run_command.sh mysql -f dump "$devdb/project.sql" "$livedb/dbconfig.php"
+~/.cmp/run_command.sh mysql -f load "$devdb/project.sql" "$devdb/dbconfig.php"
