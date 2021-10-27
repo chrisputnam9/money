d5a71f40a8168bb73c5761d4e0d13e8824a55c62
Chris Putnam <chris@webfx.com>
2021-10-27 07:08:56 -0400

    Plaid POC - In Progress

--- app/code/report/controller.php
+++ app/code/report/controller.php
@@ -15,36 +15,54 @@ class Report_Controller extends Core_Controller_Abstract
 
         if ($request->index(0,'report'))
         {
+
 			echo "<h1>Report Testing</h1>";
 
 			$current_user = Login_Helper::getCurrentUser();
 			$current_user_id = "cmp_money_" . $current_user['id'];
 
-			// Get link_token
-			$response = self::postJSON(PLAID_API_URL . "/link/token/create", [
-				'client_id' => PLAID_API_CLIENT_ID,
-				'secret' => PLAID_API_SECRET,
-				'client_name' => 'CMP Money',
-				'user' => [
-					'client_user_id' => $current_user_id,
-				],
-				'products' => ['auth'],
-				'country_codes' => ['US'],
-				'language' => 'en',
-			]);
-			self::renderLink($response['link_token']);
+			if ($request->index(1,'add_account'))
+			{
+				// Good for 30 minutes
+				$public_token = $request->post('public_token');
+				$metadata = $request->post('metadata');
 
-			// TODO
-			// Initialize Link to get public_token
-			// https://plaid.com/docs/link/
+				if (empty($public_token)) {
+					die('Missing public_token');
+				}
 
-			// TODO
-			// Exchange public_token for access_token
+				if (empty($metadata)) {
+					die('Missing metadata');
+				}
+
+				die("Public token: " . $public_token);
+
+				// TODO
+				// Exchange public_token for access_token
+
+			}
+			else
+			{
+				// Get link_token
+				$response = self::postJSON(PLAID_API_URL . "/link/token/create", [
+					'client_id' => PLAID_API_CLIENT_ID,
+					'secret' => PLAID_API_SECRET,
+					'client_name' => 'CMP Money',
+					'user' => [
+						'client_user_id' => $current_user_id,
+					],
+					'products' => ['auth'],
+					'country_codes' => ['US'],
+					'language' => 'en',
+				]);
+
+				// Initialize Link to get public_token
+				self::renderLink($response['link_token']);
+			}
 
 			// TODO
 			// Get accounts
 			// https://plaid.com/docs/quickstart/#making-api-requests
-
 
         }
     }
@@ -52,53 +70,38 @@ class Report_Controller extends Core_Controller_Abstract
 	static function renderLink($link_token)
 	{
 		?>
-		<button id="link-button">Link Account</button>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<button id="add_account_button" class='btn'>Add New Account</button>
+		<form id="add_account_form" action="/report/add_account" method="post">
+			<input type="hidden" id="public_token" name="public_token" value="" />
+			<input type="hidden" id="metadata" name="metadata" value="" />
+		</form>
 		<script src="https://cdn.plaid.com/link/v2/stable/link-initialize.js"></script>
 		<script type="text/javascript">
-		(async function($) {
-		var handler = Plaid.create({
-			// Create a new link_token to initialize Link
-		token: '<?php echo $link_token ?>',
-			onLoad: function() {
-			// Optional, called when Link loads
-			},
-			onSuccess: function(public_token, metadata) {
-			// Send the public_token to your app server.
-			// The metadata object contains info about the institution the
-			// user selected and the account ID or IDs, if the
-			// Account Select view is enabled.
-			$.post('/exchange_public_token', {
-				public_token: public_token,
-			});
-			},
-			onExit: function(err, metadata) {
-			// The user exited the Link flow.
-			if (err != null) {
-				// The user encountered a Plaid API error prior to exiting.
-			}
-			// metadata contains information about the institution
-			// that the user selected and the most recent API request IDs.
-			// Storing this information can be helpful for support.
-			},
-			onEvent: function(eventName, metadata) {
-			// Optionally capture Link flow events, streamed through
-			// this callback as your users connect an Item to Plaid.
-			// For example:
-			// eventName = "TRANSITION_VIEW"
-			// metadata  = {
-			//   link_session_id: "123-abc",
-			//   mfa_type:        "questions",
-			//   timestamp:       "2017-09-14T14:42:19.350Z",
-			//   view_name:       "MFA",
-			// }
-			}
-		});
+			const handler = Plaid.create({
+				token: '<?php echo $link_token ?>',
+				onSuccess: function(public_token, metadata) {
+					// TODO
+					console.log("public_token", public_token);
+					console.log("metadata", metadata);
 
-		$('#link-button').on('click', function(e) {
-			handler.open();
-		});
-		})(jQuery);
+					document.getElementById('')
+				},
+				onExit: function(err, metadata) {
+					// The user exited the Link flow.
+					if (err != null) {
+						// The user encountered a Plaid API error prior to exiting.
+						alert("Issue with Plaid? See Console");
+						console.group("Plaid Error");
+							console.error(err);
+							console.log(metadata);
+						console.groupEnd();
+					}
+				}
+			});
+
+			document.getElementById('add_account_button').addEventListener('click', function(e) {
+				handler.open();
+			});
 		</script>
 		<?php
 	}
